813786811f2078b8297906d1d13d753978050c2a
liying2008 <liruoer2008@yeah.net>
2022-12-02 20:40:15 +0800

    chore: remove unocss and update window config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import Unocss from 'unocss/vite'
-import presetUno from '@unocss/preset-uno'
 
 
 export const r = (...args: string[]) => resolve(__dirname, ...args)
@@ -16,12 +14,6 @@ export default defineConfig({
   },
   plugins: [
     vue(),
-    // https://github.com/unocss/unocss
-    Unocss({
-      presets: [
-        presetUno(),
-      ],
-    }),
   ],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
